112e6fd3ed186febe138715aab67a3c452bdc1d1
RektLzz <107250177+RektLzz@users.noreply.github.com>
2024-02-11 22:46:11 +0100

    Update 1.3
    Made bullet class somewhat, still WIP, but you can usually create similar bullets without copying 100 scripts + Fixed some dogshit code in the player and the dust particle. + Rearranged some files I guess

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,35 +1,61 @@
 extends Area2D
 
-const SPEED = 400
-var direction = Vector2.ZERO
+var direction : Vector2
+var velocity : float
+var damage : float
+var lifetime : float
+var target : String
+var shooter
+
+# Preload sprite
+@export var Sprite : Texture2D
+
+# Preload the trail scene
+@export var TrailParticle : PackedScene
 
 # Preload the explosion scene
-var explosionParticle = preload("res://Scenes/Effects/Explosion.tscn")
+@export var DeathParticle : PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	#direction = get_viewport().get_mouse_position()
-	#print(direction)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func _on_timer_timeout():
-	explode()
-	queue_free()
+func _on_lifetime_timeout():
+	_bulletEffect(true, DeathParticle, global_position, true, true)
 
 func _on_body_entered(body):
-	explode()
-	queue_free()
+	if body != shooter:
+		_bulletEffect(true, DeathParticle, global_position, true, true)
+		
+		if body.is_in_group(target) and body.has_method("take_damage"):
+			body.take_damage(damage)
+		
+		#print("Bullet collided with: ", body, " Shooter: ", shooter)
 	
 func _physics_process(delta):
-	position += SPEED * delta * direction
+	position += velocity * delta * direction
+	_bulletEffect(false, TrailParticle, global_position, true, false)
+
+# Play Effect
+func _bulletEffect(destroy_bullet: bool, _particleEffect: PackedScene = null, effect_position: Vector2 = Vector2.ZERO, emitting: bool = true, one_shot: bool = true):
+	# Check if a particle effect is provided
+	if _particleEffect:
+		var particles = _particleEffect.instantiate()
+		particles.position = effect_position
+		particles.emitting = emitting
+		particles.one_shot = one_shot
+		get_tree().current_scene.add_child(particles)
 	
-func explode():
-	var particle_instance = explosionParticle.instantiate()
-	particle_instance.position = global_position
-	particle_instance.one_shot = true
-	particle_instance.emitting = true
-	get_parent().add_child(particle_instance)
+	# Destroy the bullet if required
+	if destroy_bullet:
+		queue_free()
+
+func _spawn(pos, dir, vel, dmg, lft, trg, sht):
+	position = pos
+	direction = dir
+	velocity = vel
+	damage = dmg
+	lifetime = lft
+	target = trg
+	shooter = sht
+	_set_lifetime_timer()
+
+func _set_lifetime_timer():
+	$Lifetime.wait_time = lifetime
+	$Lifetime.start()
--- Scripts/Effects/DeleteEffect.gd
+++ Scripts/Effects/DeleteEffect.gd
@@ -2,17 +2,11 @@ extends CPUParticles2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Create a new Timer node
 	var timer = Timer.new()
-	# Set the timer to timeout after the lifetime of the CPUParticles2D
 	timer.wait_time = lifetime
-	# Make sure the timer does not repeat
 	timer.one_shot = true
-	# Add the timer as a child of this node
 	add_child(timer)
-	# Connect the timer's timeout signal to a method on this node using Callable
 	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
-	# Start the timer
 	timer.start()
 
 # This method will be called when the timer times out
